ddf2d8c3558ee1f6b121d0e39d4cba6b0afdbad1
nikhil <nik.autospace@gmail.com>
2018-01-22 15:56:30 +0530

    commit-5

---
+++ src/main/webapp/ui-components/ImageSlider/CenterSlider/imageSlider.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+$('.owl-carousel').owlCarousel({
+     nav:true,
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
+
+
+
+});
+  
---
+++ src/main/webapp/ui-components/ImageSlider/LazyLoaderSlider/imageSlider.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+ $('.owl-carousel').owlCarousel({
+    items:4,
+    lazyLoad:true,
+    loop:true,
+    margin:10
+});
+ 
+});

a6f4a5a6a1ec352fc403b57c8a6f3a12dbea3a6d
nikhil <nik.autospace@gmail.com>
2018-04-04 01:39:47 -0700

    bug fix for copy csv

--- src/main/webapp/ui-components/CsvToJson/js/csv.js
+++ src/main/webapp/ui-components/CsvToJson/js/csv.js
@@ -1,61 +1,65 @@
-/**
- * Taking request and providing response
- */
-
 $(document).ready(function () {
+  $("#btnSubmit").click(function (event) {
 
-    $("#btnSubmit").click(function (event) {
+    //stop submit the form, we will post it manually.
+    event.preventDefault();
 
-        //stop submit the form, we will post it manually.
-        event.preventDefault();
+    // Get form
+    var form = $('#csvForm')[0];
 
-        // Get form
-        var form = $('#csvForm')[0];
+    // Create an FormData object
+    var data = new FormData(form);
 
-		// Create an FormData object
-        var data = new FormData(form);
+    // If you want to add an extra field for the FormData
+    data.append("CustomField", "This is some extra data, testing");
 
-		// If you want to add an extra field for the FormData
-        data.append("CustomField", "This is some extra data, testing");
+    // disabled the submit button
+    $("#btnSubmit").prop("disabled", true);
 
-		// disabled the submit button
-        $("#btnSubmit").prop("disabled", true);
-    $("#copy").click(function () {
-      $("#HiddenJsonOutput").val().select();
-      document.execCommand("copy");
-      $("#copy").html("Copied");
+    $.ajax({
+      type: "POST",
+      enctype: 'multipart/form-data',
+      url: "/pepcuscapability-showcase/csv",
+      data: data,
+      processData: false,
+      contentType: false,
+      cache: false,
+      timeout: 600000,
 
-    });
-        $.ajax({
-            type: "POST",
-            enctype: 'multipart/form-data',
-            url: "/pepcuscapability-showcase/csv",
-            data: data,
-            processData: false,
-            contentType: false,
-            cache: false,
-            timeout: 600000,
-
-            success: function (data) {
-            	$(".json-data").css("display","inline-block");
-            	console.log("SUCCESS : ", data);
-            	var json = JSON.stringify(data);
-              $("#HiddenJsonOutput").html(json);
-            	$(".container").html(json);
-              $(".container").beautifyJSON({
-                hoverable: true,
-                collapsible: true
-              });
-
-                $("#btnSubmit").prop("disabled", false);
-            },
-            error: function (e) {
-                $("#result").text(e.responseText);
-                console.log("ERROR : ", e);
-                $("#btnSubmit").prop("disabled", false);
-            }
+      success: function (data) {
+        $(".json-data").css("display", "inline-block");
+        console.log("SUCCESS : ", data);
+        var json = JSON.stringify(data);
+        $("#HiddenJsonOutput").val(json);
+        $(".container").html(json);
+        $(".container").beautifyJSON({
+          hoverable: true,
+          collapsible: true
         });
 
+        $("#btnSubmit").prop("disabled", false);
+      },
+      error: function (e) {
+        $("#result").text(e.responseText);
+        console.log("ERROR : ", e);
+        $("#btnSubmit").prop("disabled", false);
+      }
     });
 
+  });
+
 });
+
+function copyFunction() {
+  var copyText = document.getElementById("HiddenJsonOutput");
+  copyText.select();
+  document.execCommand("Copy");
+
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied";
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
+}

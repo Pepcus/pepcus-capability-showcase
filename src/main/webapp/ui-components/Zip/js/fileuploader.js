9b53d3f363636ae5ea89ff9fc9df7315ea034b3a
shubham jain <shubhamjain2908@gmail.com>
2018-07-03 16:53:35 +0530

    bugs fix for zip component

--- src/main/webapp/ui-components/Zip/js/fileuploader.js
+++ src/main/webapp/ui-components/Zip/js/fileuploader.js
@@ -1,9 +1,10 @@
 $('document').ready(function(){
 
+
   $('body').niceScroll({cursorborder:"",cursorcolor:"#D2D2D2",boxzoom:true}); 
 
      $('#uploadedFiles').html('<p> Please upload a file by clicking the Upload Files button or Dragging the files in the upload section.</p>');
-    $(document).on('change','.file',function(e){
+    $(document).on('change','.file0',function(e){
         //we are using function(e) because events are not defined in firefox
         upload(e);
     });
@@ -29,6 +30,7 @@ $('document').ready(function(){
 /*****************upload file details on page***************************/
 
 function upload(event) {
+   
 
     if (window.File && window.FileList && window.FileReader) {
         var files = event.target.files; //FileList object
@@ -75,11 +77,23 @@ function uploadData(i,files){
     filesize = Math.ceil(parseInt(files[i].size)/1024);
     filetype = files[i].type;
     countbar = countbar + i;
+     /*$('#showFiles').append(    
+        '<div class = "cardview">'+
+        '<div data-id = "35654'+countbar+'">'+
+            '<div class = "file-view">'+
+                '<div class="image-thumbnail"><img src=""></div>'+
+                '<div class="file-name">'+filename+'</div>'+
+                '<div class="file-size">'+filesize+' kb</div>'+
+                '<div class="progressBar">'+
+                '<div class="progress" id="progress'+countbar+'"></div>'+
+                '</div>'+
+        '</div>');  
+        moveProgressBar($('#progress'+countbar));  */ 
 
     var reader = new FileReader();
     reader.addEventListener("load", function (event) {
         binfile = event.target.result;
-        if((/\.(gif|jpg|jpeg|tiff|png)$/i).test(filename)){ 
+        if((/\.(gif|jpg|jpeg|tiff|png)$/i).test(filename)){
             $('#showFiles').append(    
                 '<div class = "cardview">'+
                 '<div data-id = "35654'+countbar+'">'+
@@ -90,8 +104,8 @@ function uploadData(i,files){
                         '<div class="progressBar">'+
                         '<div class="progress" id="progress'+countbar+'"></div>'+
                         '</div>'+
-                        '<button class="btn preview" data-name="'+binfile+'" id="previewBtn" onclick="preview(event)">Preview</button>'+
-                        '<a href="'+binfile+'" download><button class="btn">Download</button></a>'+'</div>');
+                        ''+
+                        '<a href="'+binfile+'" download></a>'+'</div>');
            
 		}
         else{
@@ -104,15 +118,18 @@ function uploadData(i,files){
                         '<div class="file-size">'+filesize+' kb</div>'+
                         '<div class="progressBar">'+
                         '<div class="progress" id="progress'+countbar+'"></div>'+
-                        '</div>');
+                        '</div>'+
+                        ''+
+                        '<a href="'+binfile+'" download></a>'+'</div>');
         }  
         moveProgressBar($('#progress'+countbar));  
         // console.log("inside render method",filename,filesize,filetype,binfile);
-        setDataToLocal(filename,filesize,filetype,binfile);       
+        setDataToLocal(filename,filesize,filetype,binfile);
+        
     });
+
     reader.readAsDataURL(files[i]);
 }
-
 function preview(event){
     var target = $(event.currentTarget);
     var src = target.attr('data-name');
@@ -143,3 +160,148 @@ function moveProgressBar(barid){
         }
     }
 }
+
+
+/*****************Setting data to local storage**********************/
+
+function setDataToLocal(name,size,type,bindata){
+    $('#uploadedFiles').empty();
+   // $('#uploadedFiles').append('<p>Click Fetch Data button to checkout your uploaded files.</p>');
+    var dataObj = [];
+    var retrievedObject = JSON.parse(localStorage.getItem('DataObject'));
+    
+    if(retrievedObject !== null) {
+        for(i = 0 ; i < retrievedObject.length ; i++){
+            dataObj.push(retrievedObject[i]);
+        }
+    }
+        dataObj.push({
+        "filename": name,
+        "filetype": type,
+        "filesize": size,
+        "filedata":bindata
+        });
+ 
+    localStorage.setItem('DataObject', JSON.stringify(dataObj));
+}
+
+/*******************Getting data from local storage**************************/
+function getDataFromLocal(){
+    
+    var retrievedObject = localStorage.getItem('DataObject');
+    if(retrievedObject === null){
+        $('#uploadedFiles').append('<p>No files uploaded yet.</p>'+
+                    '<p>Please upload a file by clicking the Upload Files button or Dragging the files in the upload section.</p>');
+            return;
+    }
+    var datafromlocal = JSON.parse(retrievedObject);
+    for(i=0 ; i<datafromlocal.length; i++){
+        var nameoffile = datafromlocal[i].filename;
+        var dataoffile = datafromlocal[i].filedata;
+        // console.log("dataoffile",dataoffile);
+        if((/\.(gif|jpg|jpeg|tiff|png)$/i).test(nameoffile)){   
+            $('#uploadedFiles').append(
+            '<div class="upload-content">'+
+            '<div class="uploaded-image">'+
+            '<img src="'+dataoffile+'">'+
+            '<p class="uploaded-file-name">'+nameoffile+'</p>'+
+            '</div></div>');
+        }
+        else{
+           $('#uploadedFiles').append(
+            '<div class="upload-content">'+
+            '<div class="uploaded-image">'+
+            '<img src="./images/fileico.png">'+
+            '<div class="uploaded-file-name">'+nameoffile+'</div>'+
+            '</div></div>');
+        }
+    }
+}
+
+/*
+function sendDataToServer(filedataobject){
+//    console.log('filedataobject',filedataobject);
+    if(filedataobject !== null){
+            $.post(
+               // var x = "filedataobject";
+                "http://localhost/myproject/postdata.php",      
+                {data:filedataobject},
+                function(data,status){
+                    if(status === 'success'){
+                        console.log('status',status);
+                        localStorage.setItem('DataObject',null);
+                    console.log('localstorage',localStorage.getItem('DataObject'));
+                    }
+                    else{
+                        console.log('status',status);
+                        localStorage.setItem('DataObject',filedataobject);
+                        console.log('localstorage',localStorage.getItem('DataObject'));
+                    }
+                }
+            );
+        
+            }
+}
+
+function getDataFromServer(){
+    $.get('http://localhost/myproject/getdata.php',function(data){
+        var datafromserver = JSON.parse(data);
+        for(i=0 ; i<datafromserver.length; i++){
+            var nameoffile = datafromserver[i].filename;
+            var dataoffile = datafromserver[i].filedata;
+//            console.log("dataoffile",dataoffile);
+            if((/\.(gif|jpg|jpeg|tiff|png)$/i).test(nameoffile)){
+//                console.log(nameoffile);
+//                var image = document.createElement('image');
+//                image.src = dataoffile;
+                $('#uploadedFiles').append(
+                '<div class="uploaded-image">'+
+                '<img src="'+dataoffile+'"></div>');
+            }
+            else{
+               $('#uploadedFiles').append(
+                '<div class="uploaded-image">'+
+                '<img src="./../images/fileico.png"></div>');
+            }
+        }
+        
+    });
+    
+}
+
+*/
+/************************/
+//     $("#fileInput").change(function(){
+//         readURL(this);
+//     });  
+//  function readURL(input) {
+//        if (input.files && input.files[0]) {
+//            var reader = new FileReader();
+//            
+//            reader.onload = function (e) {
+//                $('#dropTarget').attr('src', e.target.result);
+//                
+//            }
+//            
+//            reader.readAsDataURL(input.files[0]);
+//        }
+//   }
+
+
+
+/*var imagereg = /\.(jpe?g|png|gif|bmp)$/i;
+        var upload = function(files){
+        var file_details = files[0];
+        console.log(file_details);
+         for(i=0; i<file_details.length; i++){
+        $('#showFiles').append(
+            '<p>name : '+file_details[i].name+'</p>'+
+            '<p>size : '+file_details[i].size+' bytes</p>'+
+            '<p>type : '+file_details[i].type+'</p>'
+       );
+             if( /\.(jpe?g|png|gif|bmp)$/i.test(file_details[i].name)){
+                alert('image');
+            }
+             
+         }
+    }*/
--- src/main/webapp/ui-components/Zip/js/zip2.js
+++ src/main/webapp/ui-components/Zip/js/zip2.js
@@ -4,31 +4,41 @@ $(document).ready(function () {
     var password = document.getElementById('key').value;
     
     var formData = new FormData();
-    
+    var f = 0;
     $.each($("input[type=file]"), function (i, obj) {                
         $.each(obj.files, function (j, file) {
             formData.append('file',file);
         	console.log(file);
+        	f++;
         });
     });
-    formData.append('key', password);
-    console.log(formData);
-        $.ajax({
-            type: 'POST',
-            url: "/pepcuscapability-showcase/zip",  // our controller
-            data: formData,
-            processData: false,                     // tell jQuery not to process the data
-            contentType: false,                     // tell jQuery not to set contentType
-            
-            success : function(data) {
-                alert('Protected Zip successfully created !!!');
-                $("#btnDownload").css("display", "block");
-                console.log(data);
-            },
-            error : function(err) {
-                console.log(err);
-            }
-        });
+    if(password.length === 0 || f === 0) {
+    	if(password.length === 0) {
+    		alert("Please provide Password for the zip");
+    	} else {
+    		alert("There are no files for creating zip");
+    	}
+    } else {
+    	formData.append('key', password);
+        console.log(formData);
+            $.ajax({
+                type: 'POST',
+                url: "/pepcuscapability-showcase/zip",  // our controller
+                data: formData,
+                processData: false,                     // tell jQuery not to process the data
+                contentType: false,                     // tell jQuery not to set contentType
+                
+                success : function(data) {
+                    alert('Protected Zip successfully created !!!');
+                    $("#btnDownload").css("display", "block");
+                    console.log(data);
+                },
+                error : function(err) {
+                    console.log(err);
+                    alert(err.responseJSON.message);
+                }
+            });
+    }
         
     });
     
